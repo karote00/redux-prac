bcc02d10c1b341f709c53122b26610efd564c4c4
ASA <karoteglathe@gmail.com>
2017-02-10 00:04:53 +0800

    update brunch config for file compile

--- brunch-config.js
+++ brunch-config.js
@@ -3,7 +3,10 @@ exports.files = {
   javascripts: {
     joinTo: {
       'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
-      'app.js': /^app/
+      'app.js': [
+      	/^app/,
+      	/^(app\/assets\/js)/
+      ]
     }
   },
   stylesheets: {joinTo: 'app.css'}
@@ -12,10 +15,20 @@ exports.files = {
 exports.plugins = {
   babel: {
   	presets: ['es2015', 'es2016', 'react'],
-  	ignore: [
-      /^(bower_components|vendor)/,
-      'app/legacyES5Code/**/*'
-    ],
-    pattern: /\.(es6|jsx)$/ // js and jsx are defaults.
+  	// ignore: [
+   //    /^(bower_components|vendor)/,
+   //    'app/legacyES5Code/**/*'
+   //  ],
+    pattern: /\.(es6|jsx|js)$/ // js and jsx are defaults.
   }
+};
+
+exports.paths = {
+	watched: [
+		'app/assets/js'
+	]
+};
+
+exports.conventions = {
+	assets: ['app/assets']
 };
