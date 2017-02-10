7d9dc076f8b9c5e9bd8ca05530e9e618e987aa4b
ASA <karoteglathe@gmail.com>
2017-02-10 19:02:49 +0800

    add todo-data store

--- app/assets/js/todo-data.js
+++ app/assets/js/todo-data.js
@@ -1,2 +1,37 @@
 import { createStore } from 'redux';
-console.log('todo-data')
+
+function todos(state = [], action) {
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state, {
+				text: action.text,
+				completed: false
+			}];
+		case COMPLETE_TODO:
+			return [
+				...state.slice(0, action.index),
+				Object.assign({}, state[action.index], {
+					completed: true
+				}),
+				...state.slice(action.index + 1)
+			];
+		case DELETE_TODO:
+			return [
+				...state.slice(0, action.index - 1),
+				...state.slice(action.index)
+			];
+		default:
+			return state;
+	}
+}
+
+
+function todoData(state = {}, action) {
+	return {
+		todos: todos(state.todos, action)
+	}
+}
+
+const tabStore = createStore(todoData);
+
+export {tabStore};
