3abd86674a65ae4a534b23fc54f9a3118be9344a
ASA <karoteglathe@gmail.com>
2017-02-11 13:53:41 +0800

    import App to initialize.js

--- app/initialize.js
+++ app/initialize.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from 'asstes/components/App';
+import App from 'components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
-  // ReactDOM.render(<)
+  ReactDOM.render(<App />, document.querySelector('#app'));
 });
