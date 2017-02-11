016c3b9763b6fd4ccb36ff33da75db80c3453f42
ASA <karoteglathe@gmail.com>
2017-02-11 22:43:39 +0800

    connect redux to props

--- app/initialize.js
+++ app/initialize.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'components/App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Todo from 'components/container';
+import todoStore from 'components/todo-reducer';
+
+let store = createStore(todoStore);
 
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
-  ReactDOM.render(<App />, document.querySelector('#app'));
+  render(
+  	<Provider store={store}>
+  		<Todo />
+  	</Provider>,
+  	document.getElementById('app')
+  );
 });
