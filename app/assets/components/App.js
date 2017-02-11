0b3f77193cf75ddb91849aa4b6d1c2f19df9b9cf
ASA <karoteglathe@gmail.com>
2017-02-11 12:53:15 +0800

    move todo to components folder

---
+++ app/assets/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { tabStore } from './todo-data';
+console.log('a')
+// console.log(tabStore)
+
+export default class App extends Component {
+	render() {
+		console.log('Here Todo')
+		return (
+			<div>I'm Todo!</div>
+		);
+	}
+}
