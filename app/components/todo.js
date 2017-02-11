620082742cef32b1dbc7d3bcea546bb7838033a5
ASA <karoteglathe@gmail.com>
2017-02-11 14:09:35 +0800

    add todo item

---
+++ app/components/todo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import todoStore from 'components/todo-data';
+
+export class Todo extends Component {
+	render() {
+		return (
+			<li>Hi</li>
+		);
+	}
+}
