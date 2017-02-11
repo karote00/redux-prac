f28563d07dc25836520ce57111c91e01c433cd5d
ASA <karoteglathe@gmail.com>
2017-02-11 12:53:39 +0800

    add main into initialize.js

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from 'asstes/components/App';
+
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
+  // ReactDOM.render(<)
 });
