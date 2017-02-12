0e925b6ecf3120b189eb1c5da7b67a9554b0de67
ASA <karoteglathe@gmail.com>
2017-02-12 22:40:09 +0800

    add todo item checkbox change handle

--- app/components/todo.js
+++ app/components/todo.js
@@ -3,11 +3,22 @@ import React from 'react';
 class Todo extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.handleCompleted = this.handleCompleted.bind(this);
+	}
+
+	handleCompleted(e) {
+		this.props.updateComplete(this.props.id);
 	}
 
 	render() {
 		return (
-			<li>{this.props.text}</li>
+			<li>
+				<div>
+					<input id={this.props.id} type="checkbox" onChange={this.handleCompleted} className="todo-checkbox" />
+					<label htmlFor={this.props.id}>{this.props.text}</label>
+				</div>
+			</li>
 		)
 	}
 }
