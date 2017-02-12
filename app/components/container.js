282b950022da56353d01b60fa1abda3a7e918134
ASA <karoteglathe@gmail.com>
2017-02-12 16:41:57 +0800

    add addTodo to mapDispatchToProps

--- app/components/container.js
+++ app/components/container.js
@@ -2,18 +2,18 @@ import TodoList from 'components/todo-list';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
-	console.log(state)
 	return { todos: state };
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-
+		addTodo: (text) => dispatch({type: 'ADD_TODO', text: text})
 	}
 }
 
 const App = connect(
-		mapStateToProps
+		mapStateToProps,
+		mapDispatchToProps
 	)(TodoList);
 
 export default App;
