0eb17c95b0d92caee716049fd8e246386a47ac14
ASA <karoteglathe@gmail.com>
2017-02-12 22:40:22 +0800

    change todo item index

--- app/components/todo-reducer.js
+++ app/components/todo-reducer.js
@@ -4,20 +4,20 @@ const todos = (state = [], action) => {
 			return [...state, {
 				text: action.text,
 				completed: false,
-				id: state.length > 0? state[state.length - 1].id + 1: 1
+				id: state.length > 0? state[state.length - 1].id + 1: 0
 			}];
 		case 'COMPLETE_TODO':
 			return [
-				...state.slice(0, action.index),
-				Object.assign({}, state[action.index], {
-					completed: true
+				...state.slice(0, action.id),
+				Object.assign({}, state[action.id], {
+					completed: !state[action.id].completed
 				}),
-				...state.slice(action.index + 1)
+				...state.slice(action.id + 1)
 			];
 		case 'DELETE_TODO':
 			return [
-				...state.slice(0, action.index - 1),
-				...state.slice(action.index)
+				...state.slice(0, action.id - 1),
+				...state.slice(action.id)
 			];
 		default:
 			return state;
